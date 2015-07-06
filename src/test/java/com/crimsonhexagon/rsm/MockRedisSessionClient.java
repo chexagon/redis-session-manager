e719087fa4cdc339c0cf7a2d524e2b4600137a75
Steve Ungerer <steve@crimsonhexagon.com>
2015-07-06 16:25:02 -0400

    mock client for test purposes

---
+++ src/test/java/com/crimsonhexagon/rsm/MockRedisSessionClient.java
@@ -0,0 +1,51 @@
+package com.crimsonhexagon.rsm;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+public class MockRedisSessionClient implements RedisSessionClient {
+
+	private ConcurrentHashMap<String, RedisSession> store = new ConcurrentHashMap<>();
+	private ConcurrentHashMap<String, Expiration> expirationTimes = new ConcurrentHashMap<>();
+	
+	@Override
+	public void initialize(RedisSessionManager manager) {
+		// noop
+	}
+
+	@Override
+	public void save(String key, RedisSession session) {
+		store.put(key, session);
+	}
+
+	@Override
+	public RedisSession load(String key) {
+		return store.get(key);
+	}
+
+	@Override
+	public void delete(String key) {
+		store.remove(key);
+	}
+
+	@Override
+	public void expire(String key, long expirationTime, TimeUnit timeUnit) {
+		expirationTimes.put(key, new Expiration(expirationTime, timeUnit)); 
+	}
+
+	@Override
+	public boolean exists(String key) {
+		return store.containsKey(key);
+	}
+	
+	public static class Expiration {
+		public final long expirationTime;
+		public final TimeUnit timeUnit;
+		
+		public Expiration(long expirationTime, TimeUnit timeUnit) {
+			this.expirationTime = expirationTime;
+			this.timeUnit = timeUnit;
+		}
+	}
+
+}
